b0b4fa8780b0b5825ff675d92d01b5f5d1fd1a47
Your Name <you@example.com>
2021-11-24 19:06:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ function pegarUserGithub(userName) {
     .then(function (resultado) {
         resultado.json().then(function (data) {
             console.log('User data: ', data)
-            mostrarUserGitHub()
+            mostrarUserGitHub(data)
+            mostrarLoginGitHub(data)
         })
     }).catch(function (erro) {
         console.log('erro: ', erro)
     })
 }
 
-function mostrarUserGitHub() {
+function mostrarUserGitHub(user) {
     if (!user) return;
     let divName = document.createElement('div');
     divName.innerText = user.userName;
     document.body.appendChild(divName);
 }
 
-function mostrarLoginGitHub() {
+function mostrarLoginGitHub(login) {
     if (!login) return;
     let divLogin = document.createElement('div');
     divLogin.innerText = user.login;
