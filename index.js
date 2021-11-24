c175a35fd7ab47c10b96a6480053b969a593aedd
Your Name <you@example.com>
2021-11-24 19:24:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,71 @@
 
+//function pegarUserGithub(userName) {
+    //fetch ('https://api.github.com/users/' + userName)
+   // .then(function (resultado) {
+      //  resultado.json().then(function (data) {
+     //       console.log('User data: ', data);
+     //       mostrarUserGitHub(data);
+      //  })
+      //  resultado.json().then(function(login) {
+      //      console.log('User login: ', login);
+      //      mostrarLoginGitHub(login);
+      //  })
+    //}).catch(function (erro) {
+      //  console.log('erro: ', erro);
+    //});
+//}
+
+//function mostrarUserGitHub(user) {
+   // if (!user) return;
+   // let divName = document.createElement('div');
+   // divName.innerText = user.userName;
+   // document.body.appendChild(divName);
+//}
+
+//function mostrarLoginGitHub(userLogin) {
+   // if (!userLogin) return;
+    //let divLogin = document.createElement('div');
+    //divLogin.innerText = userLogin.login;
+   // document.body.appendChild(divLogin);
+//}
+
+//function usarRepositoriosGithub(userName) {
+   // fetch ('https://api.github.com/users/' + userName + '/repos')
+   // .then(function (resultado) {
+    //    resultado.json().then(function (data) {
+   //         console.log('Repositories data: ', data);
+     //   });
+   // }).catch(function (erro) {
+     //   console.log('erro: ', erro);
+   // });
+//}
+
 function pegarUserGithub(userName) {
-    fetch ('https://api.github.com/users/' + userName)
-    .then(function (resultado) {
-        resultado.json().then(function (data) {
-            console.log('User data: ', data);
-            mostrarUserGitHub(data);
-        })
-        resultado.json().then(function(login) {
-            console.log('User login: ', login);
-            mostrarLoginGitHub(login);
-        })
-    }).catch(function (erro) {
-        console.log('erro: ', erro);
-    });
+    fetch('https://api.github.com/users/' + userName)
+        .then(function (resultado) {
+            resultado.json().then(function (data) {
+                console.log('User Data:', data);
+                mostrarUserGithub(data);
+            });
+        }).catch(function (erro) {
+            console.log('erro:', erro);
+        });
 }
 
-function mostrarUserGitHub(user) {
+function mostrarUserGithub(user) {
     if (!user) return;
     let divName = document.createElement('div');
-    divName.innerText = user.userName;
+    divName.innerText = user.login;
     document.body.appendChild(divName);
 }
 
-function mostrarLoginGitHub(userLogin) {
-    if (!userLogin) return;
-    let divLogin = document.createElement('div');
-    divLogin.innerText = userLogin.login;
-    document.body.appendChild(divLogin);
-}
-
-function usarRepositoriosGithub(userName) {
-    fetch ('https://api.github.com/users/' + userName + '/repos')
-    .then(function (resultado) {
-        resultado.json().then(function (data) {
-            console.log('Repositories data: ', data);
+function getUserReposGithub(userName) {
+    fetch('https://api.github.com/users/' + userName + '/repos')
+        .then(function (resultado) {
+            resultado.json().then(function (data) {
+                console.log('Repositories Data:', data);
+            });
+        }).catch(function (erro) {
+            console.log('erro:', erro);
         });
-    }).catch(function (erro) {
-        console.log('erro: ', erro);
-    });
 }
